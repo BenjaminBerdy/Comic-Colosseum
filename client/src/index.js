7dacde35b528c36e1cba4ca3cb6d6694a79ff258
Benjamin <benjamin.berdy@stonybrook.edu>
2022-03-23 13:17:46 -0400

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloProvider from './ApolloProvider'; 
 
 ReactDOM.render(
   <React.StrictMode>
