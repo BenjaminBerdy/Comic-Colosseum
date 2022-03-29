7e6b8a19e30660e6a6e7e787417b8bbda77d6099
Teghpal Singh <teghpal.singh@stonybrook.edu>
2022-03-29 14:03:42 -0400

    Created Comic Schema

---
+++ models/Comic.js
@@ -0,0 +1,22 @@
+const{ model, Schema} = require('mongoose');
+
+const comicSchema = new Schema({
+    id: String,
+    title: String,
+    author: String,
+    authorID: String,
+    publishDate: String,
+    likes: Int,
+    comments: [{type: Schema.Types.ObjectId,  ref: 'comments'}],
+    canvas: [[String]],
+    xpos:[Int],
+    ypos:[Int],
+    textFonts: [String],
+    textSizes: [Int],
+    comicText: [String],
+    textColor: [String],
+    backgroundColor: String,
+})
+
+module.exports = model('Comic', comicSchema)
+
