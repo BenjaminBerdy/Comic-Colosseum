7786d2a3530d4d20d378e5a37971cc6f6149e86b
Teghpal Singh <teghpal.singh@stonybrook.edu>
2022-03-30 13:59:50 -0400

    Fixed and Test Get Comments Finally

--- models/Comment.js
+++ models/Comment.js
@@ -4,11 +4,7 @@ const commentSchema = new Schema({
     body: String,
     username: String,
     createdAt: String,
-<<<<<<< HEAD
     comicId: String,
-=======
-    comicID: String,
->>>>>>> 687e6b9a46bbdfb3537aeebff06cbe8c6a199671
     user: {
         type: Schema.Types.ObjectId,
         ref: 'users'
