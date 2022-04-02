9bc9e686f3bb2c28268ea90a08985b661eecb747
Teghpal Singh <teghpal.singh@stonybrook.edu>
2022-04-02 18:41:43 -0400

    Split Splash Screen, css adjustments

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -4,14 +4,16 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 function SplashScreen(){
-    return(
-        <div id="splash-screen">
+    return[
+        <div id="splash-screen-left"></div>,
+        <div id="splash-screen-right">
             <div id="splash-welcome">Entering the Colosseum</div>
             <div id="splash-message">Browse, create, and interact with stories and comics. Compete for likes for yours to be on the top!
+            <br></br>
                 <Link to='/storypage/'><Button class="splash-button1">Stories</Button></Link>
                 <Link to='/comicpage/'><Button class="splash-button2">Comics</Button></Link>
             </div>
         </div>
-    )
+    ]
 }
 export default SplashScreen;
