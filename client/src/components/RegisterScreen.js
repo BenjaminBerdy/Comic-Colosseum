5a2809386d795fb8004b3fd5903408d813b9eecb
Teghpal Singh <teghpal.singh@stonybrook.edu>
2022-04-08 02:51:40 -0400

    fixed creation screen for both

--- client/src/components/RegisterScreen.js
+++ client/src/components/RegisterScreen.js
@@ -10,21 +10,85 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useNavigate } from 'react-router-dom';
 
 const theme = createTheme();
 
 export default function RegisterScreen() {
+  const [open, setOpen] = React.useState(false);
+  const [msg, setMsg] = React.useState('Enter Valid Info');
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const changeMsg = (newMsg) => {
+    setMsg(newMsg);
+  }
+
+  const navigate = useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
+      username: data.get('username'),
       email: data.get('email'),
       password: data.get('password'),
+      confirmPassword: data.get('confirmpassword')
     });
+    if (!data.get('username') || !data.get('email') || !data.get('password') || !data.get('confirmpassword')){
+      console.log("Please enter all required fields!");
+      changeMsg('Please enter all required fields!');
+      handleClickOpen();
+    }
+    else if (data.get('password').length < 8){
+      console.log("Password must be atleast 8 characters long");
+      changeMsg('Password must be atleast 8 characters long');
+      handleClickOpen();
+    }
+    else if (data.get('password') !== data.get('confirmpassword')){
+      console.log("Please enter the same password twice!");
+      changeMsg('Please enter the same password twice!');
+      handleClickOpen();
+    }
+    else navigate('/comicpage/');
+
   };
 
   return (
     <ThemeProvider theme={theme}>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Invalid Input"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {msg}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            Dismiss
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -83,7 +147,7 @@ export default function RegisterScreen() {
                   fullWidth
                   name="confirmpassword"
                   label="Confirm Password"
-                  type="confirmpassword"
+                  type="password"
                   id="confirmpassword"
                   autoComplete="confirmpassword"
                 />
@@ -107,6 +171,7 @@ export default function RegisterScreen() {
           </Box>
         </Box>
       </Container>
+      
     </ThemeProvider>
   );
 }
